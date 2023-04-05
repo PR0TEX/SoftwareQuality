5f20b078510868ec92944549cd5a0af70428fc64
pawel.domaradzki <pawel.domaradzki@dynatrace.com>
2023-04-05 21:28:58 +0200

    info about numbers added

--- bugs-detection/log1p-inaccurate.py
+++ bugs-detection/log1p-inaccurate.py
@@ -1,10 +1,11 @@
 import unittest
 import numpy
-
+# 1e-18 = 1 * 10^-18
+# 1e-18j = 1 * 10^-18 * sqrt(-1)
 class TestAccurationOfLog1pForSmallComplexInput(unittest.TestCase):
     def test(self):
         result = numpy.log1p(1e-18 + 1e-18j)
-        expected_result = 1e-18 + 1e-18j
+        expected_result = 1e-18 + 1e-18j 
         
         self.assertEqual(result, expected_result)
         
